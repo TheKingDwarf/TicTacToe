7604026cb9e3f482f0c9a800f6a03ca6051d9420
Logan Forman <35375036+TheKingDwarf@users.noreply.github.com>
2018-09-20 13:28:18 -0600

    created TicTacToe.java class

---
+++ src/TicTacToe.java
@@ -0,0 +1,17 @@
+/*
+ * 
+ * Logan Forman and Travis Dutton
+ * started:	 9/20/2018
+ * finished:
+ * Simple tic tac toe game
+ * We pledge that this program represents our own code. We received help from no-one with coding this project
+ * 
+ */
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
