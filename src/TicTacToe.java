e81bd3f4a672b2e36192ad8a035c17a0cad851ff
Logan Forman <logan.forman@sfcc.edu>
2018-09-20 19:21:38 -0600

    Added print board method

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -53,7 +53,16 @@ public class TicTacToe {
 	
 	//methods
 	
-	public 
+	/** print displays the board */
+	public static void print(String[][] m) {
+		System.out.println("\n-------------");
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
+				System.out.print("|" + m[i][j]);
+			}
+			System.out.println("|\n-------------");
+		}
+	}
 	
 
 	//main method
