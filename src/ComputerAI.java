7f747881d81c82b879826dc4b8eff39be7c42a06
BigTrav <travis.k.dutton@gmail.com>
2018-09-20 19:18:49 -0600

    Added Comment
    Added Comment

--- src/ComputerAI.java
+++ src/ComputerAI.java
@@ -1,4 +1,14 @@
 
+/*
+ * 
+ * Logan Forman and Travis Dutton
+ * started:	 9/20/2018
+ * finished:
+ * Simple computer AI 
+ * We pledge that this program represents our own code. We received help from no-one with coding this project
+ * 
+ */
+
 public class ComputerAI {
 
 }
